52d7aa9c14dd8158aa034b48629699f61a07bc6a
Nathan Kilcrease <natekilcrease@gmail.com>
2022-03-10 15:32:22 -0800

    commenting finished, finishing up readme, launching webpage

--- password_generator/README.md
+++ password_generator/README.md
@@ -1,2 +1,20 @@
 # password-generator
-javascript random password generator 
+
+In this assignment we learned how to get some information from users and apply it to something
+we also learned how to randomize something
+we learned how to do loops 
+we've learned several ways to declare something
+how to change that thing, how to rename that thing
+all in all this is very confusing
+if I was going to spend more time on this assignment i likely would have added a condition that if you didn't select a certain amount of character options that you would have to 
+start again and select the minimum amount of character options, similar to the condition added about length.
+
+### deployment and usage
+
+ click the link provided
+ select the generate button, when prompt window appears select the character sets you'd like to include
+ once finished with selections your auto generated randomized password will appear
+
+ ## Links
+
+ ## Screenshots
--- password_generator/script.js
+++ password_generator/script.js
@@ -1,16 +1,13 @@
 // Assignment Code
+// declares arrays for each character set
 var generateBtn = document.querySelector("#generate");
 
 var uppercase = ["A", "B","C", "D", "E", "F", "G", "H", "I", "J", "K", "L" ,"M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
 var symbols = ["~", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "{", "}", "|", ":", "<", ">", "?", "/", ".", ",", "'", ";", "[", "]", "=", "-"];
 
 // Write password to the #password input
-
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -22,24 +19,24 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// 1. create buckets for each character type
 
-
-
+// function that ask the user some question and then gives you a random password
 function generatePassword() {
+  // declaring an array to keep the character selections in, and a variable to asign the results to
   var passwordArray = [];
   var result = "";
   
-
+// prompt if you do not choose the required password length
 var passwordLength = prompt("Choose a password length between 8 and 128.");
 
+// if password is correct length execute code below
 if(passwordLength < 8 || passwordLength > 128) {
     alert("Please choose between 8 and 128 characters.");
 }
 
+// asks user with confirms what characters they would like to add, then adds that choice to the array
 else {
   if(confirm ("Include uppercase characters?")) {
-    // Array.prototype.push.apply(passwordArray, uppercase);
     passwordArray = passwordArray.concat(uppercase);
 }
 
@@ -55,13 +52,17 @@ else {
     passwordArray = passwordArray.concat(symbols);
 }
 
+// for loop iteration to add and randomized the selected characters  
 for(var i = 0; i < passwordLength; i++) {
     var random = Math.floor(Math.random()*passwordArray.length);
     result += passwordArray[random];
 }
 
 }
+// console.log to test results
 console.log(result);
+
+//returns the result to the user
 return result;
 }
 
